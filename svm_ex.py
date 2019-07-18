4bbeec35123e339573ca36741efb79f30a7f75bc
chliang <chliang@iii.org.tw>
2019-07-18 16:53:10 +0800

    show training log. add param verbose=True.

--- svm_ex.py
+++ svm_ex.py
@@ -5,9 +5,9 @@ X = [[0], [1], [2], [3]]
 Y = [0, 1, 2, 3]
 
 
-lin_clf = svm.LinearSVC()
+lin_clf = svm.LinearSVC(verbose=True)
 lin_clf.fit(X, Y)
 dec = lin_clf.decision_function([[1]])
 result = dec.shape[1]
-print(result)
+print('result: ', result)
 
